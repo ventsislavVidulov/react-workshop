62bce688577fc3f97089643071abfdf1e98e570d
ventsislavVidulov <xcen3k@gmail.com>
2022-07-29 16:42:46 +0300

    Lincked CSS

--- react-workshop/src/components/Header/Header.js
+++ react-workshop/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 
 const Header = () => {
-    const { user } = useContext(AuthContext);
+    //const { user } = useContext(AuthContext);
 
     return (
         <header>
@@ -14,20 +14,20 @@ const Header = () => {
                 </Link>
             </h1>
             <nav>
-                {user.email && <span>{user.email}</span>}
+                {/* {user.email && <span>{user.email}</span>} */}
                 <Link to="/catalog">All games</Link>
-                {user.email
-                    ? <div id="user">
-                        <Link to="/create">Create Game</Link>
-                        <Link to="/logout">Logout</Link>
-                    </div>
-                    : <div id="guest">
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                    </div>
-                }
+                <div id="user">
+                    <Link to="/create">Create Game</Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+                <div id="guest">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+
             </nav>
         </header>
+
     );
 };
 
